51a59c254802a09ca532b2dc3d38b538d1c7e0bc
Amelie <AmelieinInternet@163.com>
2022-06-14 04:24:13 +0800

    修复BUG

--- EVEBot/src/plugins/cmd/__init__.py
+++ EVEBot/src/plugins/cmd/__init__.py
@@ -5,6 +5,7 @@ from nonebot import on_regex
 from nonebot.plugin import export
 from nonebot.plugin import require as pluginR
 from nonebot.adapters.cqhttp import Event, Bot, Message
+from nonebot.log import logger
 
 config = nonebot.get_driver().config
 
@@ -13,14 +14,18 @@ group_ids = tool.group_ids
 
 path = os.path.abspath(os.path.dirname(__file__))
 file = open(path + '/' + 'cmd.json', 'r', encoding='utf-8')
-content = json.loads(file.read())
+try:
+    content = json.loads(file.read())
+except:
+    logger.error('cmd.json format error in plugin cmd')
+    content = {}
 file.close()
 
 load = on_regex(r'^[\.。](load)\s*')
 @load.handle()
 async def _(bot: Bot, event: Event):
     global content
-    
+
     if not (event.message_type == 'group' and event.group_id in group_ids) :
         return
     if str(event.user_id) not in config.superusers :
@@ -29,9 +34,22 @@ async def _(bot: Bot, event: Event):
             ' 不是机器人管理员，无法使用此命令！'
         ))
         return
+
     content.clear()
+
+    flag = False
+
     with open(path + '/' + 'cmd.json', 'r', encoding='utf-8') as file :
-        content = json.loads(file.read())
+        try:
+            content = json.loads(file.read())
+            flag = True
+        except:
+            logger.error('cmd.json format error in plugin cmd')
+
+    if flag :
+        await load.finish(message=Message('攻略文件加载成功！'))
+    else:
+        await load.finish(message=Message('攻略文件格式错误，加载失败！'))
 
 cmd = on_regex(r'^[\.。](cmd|a|攻略)\s*\S+')
 @cmd.handle()
@@ -40,9 +58,9 @@ async def _(bot: Bot, event: Event):
 
     if not (event.message_type == 'group' and event.group_id in group_ids) :
         return
-    
+
     s = str(event.get_message()).split(' ', 1)[1].strip()
-    
+
     if s in content :
         msg = content[s]
         await cmd.finish(message=Message(msg))
