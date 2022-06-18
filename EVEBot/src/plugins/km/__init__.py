aef6d4e4d318c7690929671d6404e040ddb74573
Amelie <AmelieinInternet@163.com>
2022-06-18 19:24:54 +0800

    为km插件增加一个异常捕获语句

--- EVEBot/src/plugins/km/__init__.py
+++ EVEBot/src/plugins/km/__init__.py
@@ -62,174 +62,177 @@ async def km():
     flag = False
 
     while True:
-        async with websockets.connect('wss://zkillboard.com/websocket/', ssl=sslContext) as websocket:
-            for v in alliance_ids :
-                sub_msg = '{' + f'"action":"sub","channel":"alliance:{v}"' + '}'
-                await websocket.send(sub_msg)
-            while True:
-                try:
-                    re = await websocket.recv()
-                except:
-                    break
-
-                new_time = time.time()
-                if new_time - old_time >= seconds_one_time :
-                    old_time = new_time
-                    send_num = 0
-
-                if new_time - km_pool_time >= km_pool_expire :
-                    km_pool_time = new_time
-                    km_pool.clear()
-
-                if send_num >= sends_per_time :
-                    continue
-
-                re_json = json.loads(re)
-
-                killID = re_json['killID']
-
-                if killID in km_pool :
-                    continue
-                else:
-                    km_pool.append(killID)
-
-                try:
-                    zkb_re = await client.get(url=f'https://zkillboard.com/api/killID/{killID}/')
-                except:
-                    continue
-                if zkb_re.status_code != 200 :
-                    continue
-                zkb_json = zkb_re.json()
-
-                loss = zkb_json[0]['zkb']['totalValue']
-
-                # 过滤小于指定损失(isk)的km
-                if loss < settings.limit : # debug
-                    continue
-
-                killHash = re_json['hash']
-                try:
-                    esi_re = await client.get(url=f'https://esi.evetech.net/latest/killmails/{killID}/{killHash}/?datasource=tranquility')
-                except:
-                    continue
-                if esi_re.status_code != 200 :
-                    continue
-                esi_json = esi_re.json()
-
-                # 过滤本联盟被击杀的km
-                if ('alliance_id' in esi_json['victim']) and (esi_json['victim']['alliance_id'] in alliance_ids) : # debug
-                    continue
-
-                msg = ''
-
-                ship_type_id = re_json['ship_type_id']
-                if tool.esi_image_server or (not Path(f'{icon_path}{ship_type_id}.png'[7:]).exists()):
-                    msg = msg + f'[CQ:image,file=https://images.evetech.net/types/{ship_type_id}/render?size=128]' + '\n'
-                else:
-                    msg = msg + f'[CQ:image,file={icon_path}{ship_type_id}.png]' + '\n'
-
-                try:
-                    ship_re = await client.get(url=f'https://esi.evetech.net/latest/universe/types/{ship_type_id}/?datasource=tranquility&language=zh', headers=headers)
-                except:
-                    continue
-                if ship_re.status_code != 200 :
-                    continue
-                ship_json = ship_re.json()
-                ship_name = ship_json['name']
-                msg = msg + f'{ship_name}\n'
-
-                dead_corporation_ticker = ''
-                dead_alliance_ticker = ''
-
-                if 'character_id' in esi_json['victim'] :
-                    dead_id = esi_json['victim']['character_id']
+        try:
+            async with websockets.connect('wss://zkillboard.com/websocket/', ssl=sslContext) as websocket:
+                for v in alliance_ids :
+                    sub_msg = '{' + f'"action":"sub","channel":"alliance:{v}"' + '}'
+                    await websocket.send(sub_msg)
+                while True:
                     try:
-                        dead_re = await client.get(url=f'https://esi.evetech.net/latest/characters/{dead_id}/?datasource=tranquility', headers=headers)
+                        re = await websocket.recv()
                     except:
-                        continue
-                    if dead_re.status_code != 200 :
-                        continue
-                    dead_json = dead_re.json()
-                    dead_name = dead_json['name']
-                    if 'corporation_id' in dead_json :
-                        try:
-                            dead_corporation_re = await client.get(url=f'https://esi.evetech.net/latest/corporations/{dead_json["corporation_id"]}/?datasource=tranquility', headers=headers)
-                        except:
-                            continue
-                        if dead_corporation_re.status_code != 200 :
-                            continue
-                        dead_corporation_json = dead_corporation_re.json()
-                        dead_corporation_ticker = '[' + dead_corporation_json['ticker'] + ']'
-                    if 'alliance_id' in dead_json :
-                        try:
-                            dead_alliance_re = await client.get(url=f'https://esi.evetech.net/latest/alliances/{dead_json["alliance_id"]}/?datasource=tranquility', headers=headers)
-                        except:
-                            continue
-                        if dead_alliance_re.status_code != 200 :
-                            continue
-                        dead_alliance_json = dead_alliance_re.json()
-                        dead_alliance_ticker = '<' + dead_alliance_json['ticker'] + '>'
-                else:
-                    dead_name = 'NPC'
-                msg = msg + f'被杀者：{dead_name} {dead_corporation_ticker}{dead_alliance_ticker}\n'
+                        break
 
-                killer_corporation_ticker = ''
-                killer_alliance_ticker = ''
+                    new_time = time.time()
+                    if new_time - old_time >= seconds_one_time :
+                        old_time = new_time
+                        send_num = 0
 
-                if zkb_json[0]['zkb']['npc'] == True :
-                    killer_name = 'NPC'
-                    continue # 过滤NPC击杀
-                else:
-                    for i in range(len(esi_json['attackers'])) :
-                        if esi_json['attackers'][i]['final_blow'] == True :
-                            killer_id = esi_json['attackers'][i]['character_id']
-                            break
-                    try:
-                        killer_re = await client.get(url=f'https://esi.evetech.net/latest/characters/{killer_id}/?datasource=tranquility', headers=headers)
-                    except:
+                    if new_time - km_pool_time >= km_pool_expire :
+                        km_pool_time = new_time
+                        km_pool.clear()
+
+                    if send_num >= sends_per_time :
                         continue
-                    if killer_re.status_code != 200 :
+
+                    re_json = json.loads(re)
+
+                    killID = re_json['killID']
+
+                    if killID in km_pool :
                         continue
-                    killer_json = killer_re.json()
-                    killer_name = killer_json['name']
-                    if 'corporation_id' in killer_json :
-                        try:
-                            killer_corporation_re = await client.get(url=f'https://esi.evetech.net/latest/corporations/{killer_json["corporation_id"]}/?datasource=tranquility', headers=headers)
-                        except:
-                            continue
-                        if killer_corporation_re.status_code != 200 :
-                            continue
-                        killer_corporation_json = killer_corporation_re.json()
-                        killer_corporation_ticker = '[' + killer_corporation_json['ticker'] + ']'
-                    if 'alliance_id' in killer_json :
-                        if killer_json['alliance_id'] not in alliance_ids :
-                            continue # 过滤非关注联盟击杀
-                        try:
-                            killer_alliance_re = await client.get(url=f'https://esi.evetech.net/latest/alliances/{killer_json["alliance_id"]}/?datasource=tranquility', headers=headers)
-                        except:
-                            continue
-                        if killer_alliance_re.status_code != 200 :
-                            continue
-                        killer_alliance_json = killer_alliance_re.json()
-                        killer_alliance_ticker = '<' + killer_alliance_json['ticker'] + '>'
                     else:
-                        continue # 过滤非关注联盟击杀
-                msg = msg + f'击杀者：{killer_name} {killer_corporation_ticker}{killer_alliance_ticker}\n'
+                        km_pool.append(killID)
 
-                msg = msg + '损失： ' + f'{loss:,.2f}' + '\n'
+                    try:
+                        zkb_re = await client.get(url=f'https://zkillboard.com/api/killID/{killID}/')
+                    except:
+                        continue
+                    if zkb_re.status_code != 200 :
+                        continue
+                    zkb_json = zkb_re.json()
 
-                if zkb_json[0]['zkb']['solo'] == True :
-                    msg = msg + 'SOLO\n'
+                    loss = zkb_json[0]['zkb']['totalValue']
 
-                msg = msg + '\n'
+                    # 过滤小于指定损失(isk)的km
+                    if loss < settings.limit : # debug
+                        continue
 
-                km_time = esi_json['killmail_time'][0:-1].split('T')
-                km_time = km_time[0] + ' ' + km_time[1]
-                msg = msg + km_time + '\n'
+                    killHash = re_json['hash']
+                    try:
+                        esi_re = await client.get(url=f'https://esi.evetech.net/latest/killmails/{killID}/{killHash}/?datasource=tranquility')
+                    except:
+                        continue
+                    if esi_re.status_code != 200 :
+                        continue
+                    esi_json = esi_re.json()
 
-                msg = msg + f'https://zkillboard.com/kill/{killID}/'
+                    # 过滤本联盟被击杀的km
+                    if ('alliance_id' in esi_json['victim']) and (esi_json['victim']['alliance_id'] in alliance_ids) : # debug
+                        continue
 
-                for v in group_ids :
-                    await bot.send_msg(message_type='group', group_id=v, message=Message(msg))
+                    msg = ''
 
-                send_num = send_num + 1
+                    ship_type_id = re_json['ship_type_id']
+                    if tool.esi_image_server or (not Path(f'{icon_path}{ship_type_id}.png'[7:]).exists()):
+                        msg = msg + f'[CQ:image,file=https://images.evetech.net/types/{ship_type_id}/render?size=128]' + '\n'
+                    else:
+                        msg = msg + f'[CQ:image,file={icon_path}{ship_type_id}.png]' + '\n'
+
+                    try:
+                        ship_re = await client.get(url=f'https://esi.evetech.net/latest/universe/types/{ship_type_id}/?datasource=tranquility&language=zh', headers=headers)
+                    except:
+                        continue
+                    if ship_re.status_code != 200 :
+                        continue
+                    ship_json = ship_re.json()
+                    ship_name = ship_json['name']
+                    msg = msg + f'{ship_name}\n'
+
+                    dead_corporation_ticker = ''
+                    dead_alliance_ticker = ''
+
+                    if 'character_id' in esi_json['victim'] :
+                        dead_id = esi_json['victim']['character_id']
+                        try:
+                            dead_re = await client.get(url=f'https://esi.evetech.net/latest/characters/{dead_id}/?datasource=tranquility', headers=headers)
+                        except:
+                            continue
+                        if dead_re.status_code != 200 :
+                            continue
+                        dead_json = dead_re.json()
+                        dead_name = dead_json['name']
+                        if 'corporation_id' in dead_json :
+                            try:
+                                dead_corporation_re = await client.get(url=f'https://esi.evetech.net/latest/corporations/{dead_json["corporation_id"]}/?datasource=tranquility', headers=headers)
+                            except:
+                                continue
+                            if dead_corporation_re.status_code != 200 :
+                                continue
+                            dead_corporation_json = dead_corporation_re.json()
+                            dead_corporation_ticker = '[' + dead_corporation_json['ticker'] + ']'
+                        if 'alliance_id' in dead_json :
+                            try:
+                                dead_alliance_re = await client.get(url=f'https://esi.evetech.net/latest/alliances/{dead_json["alliance_id"]}/?datasource=tranquility', headers=headers)
+                            except:
+                                continue
+                            if dead_alliance_re.status_code != 200 :
+                                continue
+                            dead_alliance_json = dead_alliance_re.json()
+                            dead_alliance_ticker = '<' + dead_alliance_json['ticker'] + '>'
+                    else:
+                        dead_name = 'NPC'
+                    msg = msg + f'被杀者：{dead_name} {dead_corporation_ticker}{dead_alliance_ticker}\n'
+
+                    killer_corporation_ticker = ''
+                    killer_alliance_ticker = ''
+
+                    if zkb_json[0]['zkb']['npc'] == True :
+                        killer_name = 'NPC'
+                        continue # 过滤NPC击杀
+                    else:
+                        for i in range(len(esi_json['attackers'])) :
+                            if esi_json['attackers'][i]['final_blow'] == True :
+                                killer_id = esi_json['attackers'][i]['character_id']
+                                break
+                        try:
+                            killer_re = await client.get(url=f'https://esi.evetech.net/latest/characters/{killer_id}/?datasource=tranquility', headers=headers)
+                        except:
+                            continue
+                        if killer_re.status_code != 200 :
+                            continue
+                        killer_json = killer_re.json()
+                        killer_name = killer_json['name']
+                        if 'corporation_id' in killer_json :
+                            try:
+                                killer_corporation_re = await client.get(url=f'https://esi.evetech.net/latest/corporations/{killer_json["corporation_id"]}/?datasource=tranquility', headers=headers)
+                            except:
+                                continue
+                            if killer_corporation_re.status_code != 200 :
+                                continue
+                            killer_corporation_json = killer_corporation_re.json()
+                            killer_corporation_ticker = '[' + killer_corporation_json['ticker'] + ']'
+                        if 'alliance_id' in killer_json :
+                            if killer_json['alliance_id'] not in alliance_ids :
+                                continue # 过滤非关注联盟击杀
+                            try:
+                                killer_alliance_re = await client.get(url=f'https://esi.evetech.net/latest/alliances/{killer_json["alliance_id"]}/?datasource=tranquility', headers=headers)
+                            except:
+                                continue
+                            if killer_alliance_re.status_code != 200 :
+                                continue
+                            killer_alliance_json = killer_alliance_re.json()
+                            killer_alliance_ticker = '<' + killer_alliance_json['ticker'] + '>'
+                        else:
+                            continue # 过滤非关注联盟击杀
+                    msg = msg + f'击杀者：{killer_name} {killer_corporation_ticker}{killer_alliance_ticker}\n'
+
+                    msg = msg + '损失： ' + f'{loss:,.2f}' + '\n'
+
+                    if zkb_json[0]['zkb']['solo'] == True :
+                        msg = msg + 'SOLO\n'
+
+                    msg = msg + '\n'
+
+                    km_time = esi_json['killmail_time'][0:-1].split('T')
+                    km_time = km_time[0] + ' ' + km_time[1]
+                    msg = msg + km_time + '\n'
+
+                    msg = msg + f'https://zkillboard.com/kill/{killID}/'
+
+                    for v in group_ids :
+                        await bot.send_msg(message_type='group', group_id=v, message=Message(msg))
+
+                    send_num = send_num + 1
+        except:
+            pass
